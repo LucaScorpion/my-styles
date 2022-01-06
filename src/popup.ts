11583de19b3e684a226e87ca6556033921968097
Luca Scalzotto <luca@scorpiac.com>
2022-01-06 21:13:36 +0100

    Add popup css

--- src/popup.ts
+++ src/popup.ts
@@ -1,3 +1,13 @@
 (() => {
-  document.getElementById('log')!.addEventListener('click', () => console.log('CLICKED!'));
+  document.getElementById('log')!.addEventListener('click', async () => {
+    console.log('CLICKED!');
+
+    await browser.storage.sync.set({
+      styles: [
+        'https://gist.githubusercontent.com/LucaScorpion/6dd6a9b74e8326e420ed8d2a1f0a4635/raw/29151b4f58003e898642e5769b1e92502de64989/infi.nl.css',
+      ],
+    });
+    console.log(await browser.storage.sync.getBytesInUse());
+    console.log(await browser.storage.sync.get());
+  });
 })();
